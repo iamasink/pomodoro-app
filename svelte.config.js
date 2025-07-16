73b5fe7e99218be733e8bc18176e3642b129ed38
iamasink <iamasink@proton.me>
2025-07-16 21:59:23 +0100

    gh pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,14 @@
-import adapter from "@sveltejs/adapter-auto";
-import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import adapter from "@sveltejs/adapter-static"
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte"
 
 const config = {
   preprocess: vitePreprocess(),
-  kit: { adapter: adapter() },
-};
+  kit: {
+    adapter: adapter(),
+    paths: {
+      base: process.env.NODE_ENV === 'production' ? '/pomodoro-app' : '',
+    }
+  },
+}
 
-export default config;
+export default config
