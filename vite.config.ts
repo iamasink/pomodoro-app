412a18b7977e1637c8419b5162c1d991525311de
iamasink <iamasink@proton.me>
2025-07-18 18:01:25 +0100

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { sveltekit } from "@sveltejs/kit/vite";
-import { defineConfig } from "vite";
-import devtoolsJson from "vite-plugin-devtools-json";
+import { sveltekit } from "@sveltejs/kit/vite"
+import { defineConfig } from "vite"
+import devtoolsJson from "vite-plugin-devtools-json"
 
 export default defineConfig({
   plugins: [sveltekit(), devtoolsJson()],
-});
+  base: "/pomodoro-app"
+})
